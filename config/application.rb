4e5935015e6b5544d9654adf6063b1f88467e5b7
choco-chai <umvienta@gmail.com>
2020-10-15 21:14:30 +0900

    修正6

--- config/application.rb
+++ config/application.rb
@@ -10,26 +10,6 @@ module Furima30543
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    config.i18n.default_locale = :ja
-=======
->>>>>>> parent of c128b17... revert2
-=======
-=======
-    config.i18n.default_locale = :ja
->>>>>>> parent of fa50f59... テストコード実装
->>>>>>> parent of cc505b5... Revert "revert2"
-=======
-=======
-    config.i18n.default_locale = :ja
->>>>>>> parent of fa50f59... テストコード実装
->>>>>>> parent of cc505b5... Revert "revert2"
-=======
->>>>>>> parent of 82328b4... Revert "Revert "revert2""
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
