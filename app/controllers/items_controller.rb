865d2fb6fb9f1ebcac88b55b57b26eb7e384cec5
choco-chai <umvienta@gmail.com>
2020-10-19 19:22:54 +0900

    コントローラー、ルーティング編集　商品編集ページ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,7 +20,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,4 +41,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :text, :price, :genre_id, :item_status_id, :delivery_charge_id, :state_id, :delivery_days_id, :image).merge(user_id: current_user.id)
   end
   
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
