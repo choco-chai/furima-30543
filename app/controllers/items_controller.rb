ca34fa5cc263fa50324a324443a93ceb6fae1e11
choco-chai <umvienta@gmail.com>
2020-10-30 18:40:52 +0900

    画像プレビュー機能、複数枚投稿機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,7 +43,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :text, :price, :genre_id, :item_status_id, :delivery_charge_id, :state_id, :delivery_days_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title, :text, :price, :genre_id, :item_status_id, :delivery_charge_id, :state_id, :delivery_days_id, images: []).merge(user_id: current_user.id)
   end
 
   def set_item
